e143ea81dd6839ddd6ce431b803dd4c958e5ade8
Eldan Abdrashim <eldan.abdrashim@gmail.com>
2018-09-20 00:24:04 +0600

    Leetcode added additional sulution

--- Problems/leetcode/tree/Validate_Binary_Search_Tree_98.py
+++ Problems/leetcode/tree/Validate_Binary_Search_Tree_98.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/validate-binary-search-tree/description/
 
 """
-Solution.
+1st solution.
 Complexity analysis:
 Time: O(N) - always
 Memory: O(N) - always
@@ -32,3 +32,26 @@ class Solution:
         self.inOrder(root.left, output)
         output.append(root.val)
         self.inOrder(root.right, output)
+
+"""
+2nd solution, more concise.
+Complexity analysis:
+Time: O(N) - always
+Memory: O(N) - always
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root, minVal = float('inf'), maxVal = float('-inf')):
+        if not root:
+            return True
+        if root.val <= maxVal or root.val >= minVal:
+            return False
+        return self.isValidBST(root.left, min(root.val, minVal), maxVal) and \
+            self.isValidBST(root.right, minVal, max(root.val, maxVal))
