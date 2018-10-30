9b51bd80a54661f08f3d5fa36830fbb1d4ac0d9b
Eldan Abdrashim <eldan.abdrashim@gmail.com>
2018-10-30 22:28:22 +0600

    Daily Coding Challenge added

--- Problems/EPI/Ch 05. Arrays/5-02-Increment_an_Arbitrary-Precision_integer.py
+++ Problems/EPI/Ch 05. Arrays/5-02-Increment_an_Arbitrary-Precision_integer.py
@@ -8,14 +8,19 @@ Memory: O(1) - always
 def plus_One(A):
 	A[-1] += 1
 
-	for i in reversed(range(len(A)):
+	for i in reversed(range(1, len(A))):
 		if A[i] != 10:
 			break
 		A[i] = 0
 		A[i - 1] += 1
-	
+
 	if A[0] == 10:
 		A[0] = 1
 		A.append(0)
 
 	return A
+
+if __name__ == '__main__':
+	A = [9, 9]
+
+	print(plus_One(A))
---
+++ Problems/daily-coding-problem/Problem_1.py
@@ -0,0 +1,28 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+"""
+
+
+def find_target(A, target):
+	data = {}
+	for num in A:
+		temp = target - num
+		if data.get(temp):
+			return True
+
+		data[num] = 1
+
+	return False
+
+
+if __name__ == '__main__':
+	A = [10, 15, 3, 7]
+	target = 17
+
+	print(find_target(A, target))
