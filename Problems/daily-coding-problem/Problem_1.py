10807dc3c7800dfa2735270e699f116b15d040cc
Eldan Abdrashim <eldan.abdrashim@gmail.com>
2018-10-31 10:40:18 +0600

    Daily coding problem added

--- Problems/daily-coding-problem/Problem_1.py
+++ Problems/daily-coding-problem/Problem_1.py
@@ -8,6 +8,12 @@ Given a list of numbers and a number k, return whether any two numbers from the 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 """
 
+"""
+Solution.
+Complexity analysis:
+Time: O(N) - in worst case
+Memory: O(N) - in worst case
+"""
 
 def find_target(A, target):
 	data = {}
---
+++ Problems/daily-coding-problem/Problem_2.py
@@ -0,0 +1,42 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+"""
+
+"""
+Solution.
+Complexity analysis:
+Time: O(N) - always
+Memory: O(N) - always
+"""
+
+def multiply(A):
+	n = len(A)
+	result = [0] * n
+	product = 1
+
+	for i in range(n):
+		result[i] = product
+		product *= A[i]
+
+	product = 1
+	for i in reversed(range(n)):
+		result[i] *= product
+		product *= A[i]
+
+
+	return result
+
+if __name__ == '__main__':
+	A = [1, 2, 3, 4, 5]
+	
+	print('Product of Array Except Self')
+	print('Before: {}'.format(A))
+	print('After: {}'.format(multiply(A)));
