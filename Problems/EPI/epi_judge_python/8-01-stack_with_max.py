5b441f64bd1009bd354937e4f9a6133e13b57692
Eldan Abdrashim <eldan.abdrashim@gmail.com>
2018-11-27 22:21:40 +0600

    EPI 8th chapter solving

--- Problems/EPI/epi_judge_python/8-01-stack_with_max.py
+++ Problems/EPI/epi_judge_python/8-01-stack_with_max.py
@@ -3,21 +3,45 @@ from test_framework.test_failure import TestFailure
 
 
 class Stack:
+    class MaxWithCount:
+        def __init__(self, max, count):
+            self.max = max
+            self.count = count
+
+    def __init__(self):
+        self._elements = []
+        self._cached_max_with_count = []
+
     def empty(self):
-        # TODO - you fill in here.
-        return True
+        return len(self._elements) == 0
 
     def max(self):
-        # TODO - you fill in here.
-        return 0
+        if self.empty():
+            raise IndexError('max(): empty stack')
+        return self._cached_max_with_count[-1].max
 
     def pop(self):
-        # TODO - you fill in here.
-        return 0
+        if self.empty():
+            raise IndexError('pop(): empty stack')
+        pop_element = self._elements.pop()
+        current_max = self._cached_max_with_count[-1].max
+        if pop_element == current_max:
+            self._cached_max_with_count[-1].count -= 1
+            if self._cached_max_with_count[-1].count == 0:
+                self._cached_max_with_count.pop()
+
+        return pop_element
 
     def push(self, x):
-        # TODO - you fill in here.
-        return
+        self._elements.append(x)
+        if len(self._cached_max_with_count) == 0:
+            self._cached_max_with_count.append(self.MaxWithCount(x, 1))
+        else:
+            current_max = self._cached_max_with_count[-1].max
+            if x == current_max:
+                self._cached_max_with_count[-1].count += 1
+            elif x > current_max:
+                self._cached_max_with_count.append(self.MaxWithCount(x, 1))
 
 
 def stack_tester(ops):
@@ -52,5 +76,5 @@ def stack_tester(ops):
 
 if __name__ == '__main__':
     exit(
-        generic_test.generic_test_main("stack_with_max.py",
+        generic_test.generic_test_main("8-01-stack_with_max.py",
                                        'stack_with_max.tsv', stack_tester))
