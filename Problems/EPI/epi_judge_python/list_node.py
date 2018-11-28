9451bf58366f594c8ce20f883eb3820746f2ebca
Eldan Abdrashim <eldan.abdrashim@gmail.com>
2018-11-28 22:54:20 +0600

    EPI 8th solution added

--- Problems/EPI/epi_judge_python/list_node.py
+++ Problems/EPI/epi_judge_python/list_node.py
@@ -1,12 +1,7 @@
-
-
 class ListNode:
     def __init__(self, data=0, next=None):
         self.data = data
         self.next = next
-
-
-
 
     def __eq__(self, other):
         a, b = self, other
