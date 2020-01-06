f7b845af6e2b4f5515496455240d2e4d51fc903a
Eldan Abdrashim <eldan.abdrashim@gmail.com>
2020-01-07 00:00:07 +0600

    Bolt interview questions added

---
+++ Problems/companies/Bolt/find_target_num.py
@@ -0,0 +1,5 @@
+"""
+In an array of non-negative intergers find a sequence of integers
+which sum up to the given number. I.e. [1, 2, 3], find 5 -> return [2, 3]
+"""
+
---
+++ Problems/companies/Bolt/same_char_num.py
@@ -0,0 +1,5 @@
+"""
+Check if string contains the exact same number of repetitions of every character. I.e. "aabb" - true, "aabbc" - false.
+Then check if removing a single character from the string can make it valid, i.e. "aabbc" - true, "aaabbbcc" - false
+
+"""
